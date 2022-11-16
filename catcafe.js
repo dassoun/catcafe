6410667dfbbfe2af425dd4780c7054ecc182a123
Julien Coignet <jcoignet@coyote-group.com>
2022-11-16 15:31:52 +0100

    translatable tooltips

--- catcafe.js
+++ catcafe.js
@@ -18,8 +18,8 @@
 define([
     "dojo","dojo/_base/declare", "dojo/query", "dojo/aspect", "dojo/dom", "dojo/_base/connect",
     "ebg/core/gamegui",
-    "ebg/counter", 
-    g_gamethemeurl + "./modules/CTCTooltips.js"
+    "ebg/counter"//, 
+    //g_gamethemeurl + "./modules/CTCTooltips.js"
 ],
 function (dojo, declare) {
     return declare("bgagame.catcafe", ebg.core.gamegui, {
@@ -30,6 +30,25 @@ function (dojo, declare) {
             // Example:
             // this.myGlobalValue = 0;
 
+            this.tooltip_shape = [
+                _("CAT HOUSE: After you mark a house on the sheet, choose a Cat that has not yet been 'scored'. You then immediately score 2 points for each of the items you've " +
+                "drawn on your sheet that match the item shown with the cat. Write down the number of points in the box below that cat. That cat is now considered 'scored' " +
+                "and cannot be scored again."),
+            
+                _("BALL OF YARN: At the end of the game, count the number of Balls of Yarn in each Cat Tower. Players with the most in each Cat Tower get 8 points. If a " +
+                "player doesn’t have the most but still has at least one Ball of Yarn, they score 3 points for that Cat Tower. If Players tie for the most Balls of Yarn in a "+
+                "Cat Tower, each tied player gets 8 points."),
+            
+                _("BUTTERFLY TOY: A Butterfly Toy gives you two unmarked footprints. These footprints are then considered available for " +
+                "you to use in future rounds. At the end of the game, you score 3 points for each Butterfly Toy."),
+            
+                _("FOOD BOWL: At the end of the game, Food bowls score 1 point for each different adjacent item."),
+            
+                _("CUSHION: At the end of the game, each Cushion scores points equal to the floor number it is drawn in."),
+            
+                _("MOUSE TOY: At the end of the game, you receive 2/6/12/20 points depending on the number of adjacently connected Mouse Toys in a 'chain'. Each " + 
+                "chain of Mouse Toys can score a maximum of 20 points. Each Mouse Toy can only be scored for one chain, however, multiple chains may be drawn.")
+            ];
         },
         
         /*
@@ -177,7 +196,7 @@ function (dojo, declare) {
 
                     this.slideToObjectPos( $('shape_selection_'+player_id+'_'+i), $('ctc_player_board_'+player_id), this.getXPixelCoordinatesShapeSelection(i), this.gameConstants['SHAPE_SELECTION_Y_ORIGIN'], 10 ).play();
 
-                    this.addTooltip( 'shape_selection_'+player_id+'_'+i, _(tooltip_shape[i-1]), '' );
+                    this.addTooltip( 'shape_selection_'+player_id+'_'+i, _(this.tooltip_shape[i-1]), '' );
                 }
 
                 // Cat footprints
